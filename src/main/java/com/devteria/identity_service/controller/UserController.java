04c88bab2324173831f04b41d0e5f77fc3ad5dc7
sonpn-cmp <sonpn@comacpro.vn>
2026-03-10 23:54:20 +0700

    first commit

---
+++ src/main/java/com/devteria/identity_service/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.devteria.identity_service.controller;
+
+import com.devteria.identity_service.dto.request.UserCreationRequest;
+import com.devteria.identity_service.entity.User;
+import com.devteria.identity_service.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    User createUser(@RequestBody UserCreationRequest request){
+        return userService.createUser(request);
+    }
+
+    @GetMapping
+    List<User> getUsers(){
+        return userService.getUsers();
+    }
+}
